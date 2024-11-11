7e597124293a95276a51f032bde5ffaa7ad85e8f
costamurilo <42807980+costamurilo@users.noreply.github.com>
2024-11-11 16:50:40 -0300

    Update lnxpre.sh
    Versao nova

--- lnxpre.sh
+++ lnxpre.sh
@@ -1,114 +1,282 @@
 #!/bin/bash
 
+ # Criado por : Murilo Costa
+ # Email : murilo.c.costa@oracle.com
+ # Data : 17/02/2023 
+
+
+ # Pre requisitos
+ # Criar vg com minimo de 700GB
+ # Script interativo.
+ # Executar estes comandos antes de executar o script
+ # mkdir -p /tmp/sg
+ # cd /tmp/sg
+ # echo "/ocisg          ocisg    10 " >> /tmp/sg/lista.lst
+ # echo "/ocisg/cache    cache    500" >> /tmp/sg/lista.lst
+ # echo "/ocisg/metadata metadata 100" >> /tmp/sg/lista.lst
+ # echo "/ocisg/log      log      30 " >> /tmp/sg/lista.lst
+
+ # Instalacao do Storage gateway
+ # Filesystem da instacao : /ocisg
+ # Filesystem do cache    : /ocisg/cache
+ # Filesystem do metadata : /ocisg/metadata
+ # Filesystem do log      : /ocisg/log
+
+
+ # Set de variaveis
+
+ HOMEDIR="/tmp/sg"
+   LISTA="${HOMEDIR}/lista.lst"
+    NAME="ocisg cache metadata log"
+     FSS="/ocisg /ocisg/cache /ocisg/metadata /ocisg/log"
+
+ ######################################################################################################################
  clear
+
+ # Funcoes
+ fn_rc(){
+ 	     if [ ${?} -eq 0 ]
+ 	     	 then
+ 	     	 	 echo "OK"
+ 	     	 else
+ 	     	 	 echo "NOK"
+ 	     fi
+ }
+
+ val_vg(){
+ 	   echo " Validando VG \"${VGNAME}\" "
+ 	   sleep 1
+
+ 	   VALIDA="$(vgs | grep -w ${VGNAME} | wc -l)"
+ 	   if [ ${VALIDA} -eq 0 ]
+ 	     then
+ 	     	 echo ""
+ 	     	 echo " Vg \"${VGNAME}\" inexistente."
+ 	     	 sleep 1
+ 	     	 echo ""
+ 	     	 echo -e " Verifique o nome do VG e digite novamente.\c"
+ 	     	 sleep 1
+ 	     	 echo " Fim! "
+ 	     	 exit 100
+ 	     else
+ 	     	 VGSIZE="$(vgs | grep -w ${VGNAME} | awk '{ print $6 }' | cut -f 2 -d \< | cut -f 1 -d .)"
+ 	     	 if [ ${VGSIZE} -gt 700 ]
+ 	     	 	 then
+ 	     	 	 	 echo " Volume do VG \"${VGNAME}\" OK! [ ${VGSIZE} ]"
+ 	     	 	 	 sleep 1
+    	     	 echo " VG \"${VGNAME}\" validado!"
+    	     	 sleep 1
+ 	     	     echo ""
+ 	     	   else
+ 	     	   	 echo " Volume do VG \"${VGNAME}\" insuficiente : [ ${VGSIZE} ] "
+ 	     	   	 echo " Para continuar utilize um VG com pelo menos 700GB."
+ 	     	   	 echo " Fim."
+ 	     	   	 #exit 200
+ 	     	 fi
+ 	   fi
+ }
+
+ val_dir(){
+         echo ""
+         echo " Checando se o dir [/tmp/sg ] existe : "
+         sleep 1
+         DIR="/tmp/sg"
+         if [  -e ${DIR} ]
+           then
+           	 echo " Diretorio [ /tmp/sg ] existente."
+           	 sleep 1
+           	 echo  -e " Limpando diretorio : \c"
+           	 unalias rm > /dev/null 2>&1
+           	 rm -f ${DIR}/* > /dev/null 2>&1
+           	 sleep 1
+           	 fn_rc
+           else
+           	 echo " Diretorio [ /tmp/sg ] nao existente."
+           	 echo  -e " Criando diretorio : \c"
+           	 mkdir -p /tmp/sg > /dev/null 2>&1
+           	 fn_rc
+         fi
+
+ }
+
+ ######################################################################################################################
+ # Execucao do script
+ 
+ # Valida o diretorio /tmp/sg
+ val_dir
+
  echo ""
- echo "- ------------------------------------------------------------------- -"
- echo "-                         Linux Pre Reqs "
- echo "- ------------------------------------------------------------------- -"
+ vgs
+ echo ""
 
- fn_rc()
- {
-   if [ ${?} -eq 0 ]
-     then
-     	 echo "OK"
-     else
-     	 echo "NOK"
-     	 exit 100
-   fi
- }
+ echo -e " Digite o nome do VG onde a estrutura do Storage Gateway deve ser criado : \c"
+ read VGNAME
+ echo ""
+ 
+ # Valida o tamanho do VG
+ val_vg
 
- fn_grub_version()
- {
- 	 echo -e " - Testando versao do Grub : \c"
-   	which grub-install > /dev/null 2>&1
+ # Coleta o tamanho desejado de cada filesystem
 
-   if [ ${?} -ne 0 ]
-   	 then
-   	 	 which grub2-install > /dev/null 2>&1
-   	 	 if [ ${?} -eq 0 ]
-   	 	 	 then
-   	 	 	 	 sleep 1
-   	 	 	 	 echo -e "grub2 -> \c"
-   	 	 	 	 sleep 1
-   	 	 	 	 which grub2-install
-   	 	 	 	 VGRUB=2
-   	 	 	 else
-   	 	 	 	 echo "Versao do Grub nao identificada."
-   	 	 fi
-   	 else
-   	 	 sleep 1
-   	 	 echo -e "grub -> \c"
-   	 	 sleep 1
-   	 	 which grub-install
-   	 	 VGRUB=1
-   fi
- }
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg\" [ 10 ] : \c"
+ read OCISG_SIZE
+ if [ -z ${OCISG_SIZE} ]
+ 	 then
+ 	 	 OCISG_SIZE="10"
+ fi
+
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg/cache\" [ 500 ] : \c"
+ read OCISG_CACHE_SIZE
+ if [ -z ${OCISG_CACHE_SIZE} ]
+ 	 then
+ 	 	 OCISG_CACHE_SIZE="500"
+ fi
+
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg/metadata\" [ 100 ] : \c"
+ read OCISG_METADATA_SIZE
+ if [ -z ${OCISG_METADATA_SIZE} ]
+ 	 then
+ 	 	 OCISG_METADATA_SIZE="100"
+ fi
+
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg/log\" [ 30 ] : \c"
+ read OCISG_LOG_SIZE
+ if [ -z ${OCISG_LOG_SIZE} ]
+ 	 then
+ 	 	 OCISG_LOG_SIZE="30"
+ fi
+
+ echo ""
+ # --------------------------------------------------------------------------------------------------------------------
 
 
- fn_grub_config()
- {
- 	 case ${VGRUB} in
-     2)
-       echo ""
-       echo " - Configurando grub2-install."
-       echo -e " - Checando arquivo \"/etc/default/grub\" :  \c"
-       CHKFILE="$(ls /etc/default/grub | wc -l 2>/dev/null)"
-       if [ ${CHKFILE} -eq 0 ]
-       	 then
-       	 	 echo "NOK - Arquivo de config nao encontrado."
-       	 	 exit 10
-       	 else
-       	 	 sleep 1
-       	 	 echo "OK - Arquivo encontrado."
-           echo -e " - Executando backup de \"/etc/default/grub\" em /tmp : \c"
-           sleep 1
-           tar -cvf /tmp/etc_default_grub.tar /etc/default/grub > /dev/null 2>&1
-           fn_rc
-           
-           echo -e " - Gerando novo arquivo grub : \c"
-           sleep 1
 
-           cat /etc/default/grub | grep -v GRUB_CMDLINE_LINUX >> /tmp/grub 2>/dev/null
-           echo "GRUB_SERIAL_COMMAND=\"serial --unit=0 --speed=115200\"" >> /tmp/grub 2>/dev/null 
-           echo "GRUB_TERMINAL=\"serial console\"" >> /tmp/grub 2>/dev/null 
-           echo "GRUB_CMDLINE_LINUX=\"console=tty1 console=ttyS0,115200\"" >> /tmp/grub 2>/dev/null 
-           cat /tmp/grub > /etc/default/grub 2>/dev/null
 
-           grub2-mkconfig -o /boot/grub2/grub.cfg 2>/dev/null
 
-           fn_rc
-       fi
+ # Criacao dos LVs
+ echo " - Criando estrutura de Filesystem do Storage Gateway"
+ echo ""
 
+ for LV in ${NAME}
+ do
+   case ${LV} in
+   	 ocisg)
+       echo " - Criando LV lv_sg_${LV} "
+       sleep 1
+       lvcreate -y -L${OCISG_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
+       
      ;;
 
-     1)
+     cache)
+       echo  " - Criando LV lv_sg_${LV} "
+       sleep 1
+       lvcreate -y -L${OCISG_CACHE_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
+       
+     ;;
+
+       metadata)
+       echo  " - Criando LV lv_sg_${LV}  "
+       sleep 1
+       lvcreate -y -L${OCISG_METADATA_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
+       
+     ;;
+
+       log)
+       echo  " - Criando LV lv_sg_${LV} "
+       sleep 1
+       lvcreate -y -L${OCISG_LOG_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
        
      ;;
 
      *)
-       echo " [ ERR ] - Versao de grub nao encontrada"
+       echo " - Opcao desconhecida."
+     ;;
+   esac
+ done
+ 
+ echo ""
+ echo " - Criando estrutura de filesystems"
+ echo ""
+
+ # Criacao dos mountpoints / mountagem dos FS / composicao do fstab
+ for FS in ${FSS}
+ do
+   case ${FS} in
+   	 /ocisg)
+       echo  " - Criando FS ${FS}  "
+       sleep 1
+       mkdir -p ${FS} > /dev/null 2>&1
+       unset LV
+       LV="/dev/mapper/${VGNAME}-lv_sg_ocisg"
+       mount ${LV} ${FS}  > /dev/null 2>&1
+       
+
+       echo  " - Compondo /etc/fstab  "
+       $(echo "/dev/mapper/${VGNAME}-lv_sg_ocisg    ${FS}    xfs    defaults 0  0" >> /etc/fstab)
+       echo ""
+     ;;
+
+     /ocisg/cache)
+       echo  " - Criando FS ${FS}  "
+       sleep 1
+       mkdir -p ${FS} > /dev/null 2>&1
+       unset LV
+       LV="/dev/mapper/${VGNAME}-lv_sg_cache"
+       mount ${LV} ${FS} 
+       
+
+       echo  " - Compondo /etc/fstab  "
+       $(echo "/dev/mapper/${VGNAME}-lv_sg_cache    ${FS}    xfs    defaults 0  0" >> /etc/fstab)
+       echo ""
        
      ;;
 
+       /ocisg/metadata)
+       echo  " - Criando FS ${FS}  "
+       sleep 1
+       mkdir -p ${FS} > /dev/null 2>&1
+       unset LV
+       LV="/dev/mapper/${VGNAME}-lv_sg_metadata"
+       mount ${LV} ${FS} > /dev/null 2>&1
+       
+
+       echo  " - Compondo /etc/fstab  "
+       $(echo "/dev/mapper/${VGNAME}-lv_sg_metadata    ${FS}    xfs    defaults 0  0" >> /etc/fstab)
+       echo ""
+       
+     ;;
+
+       /ocisg/log)
+       echo  " - Criando FS ${FS}  "
+       sleep 1
+       mkdir -p ${FS} > /dev/null 2>&1
+       unset LV
+       LV="/dev/mapper/${VGNAME}-lv_sg_log"
+       mount ${LV} ${FS} > /dev/null 2>&1
+       
+
+       echo  " - Compondo /etc/fstab  "
+       $(echo "/dev/mapper/${VGNAME}-lv_sg_log    ${FS}    xfs    defaults 0  0" >> /etc/fstab)
+       echo ""
+       
+     ;;
+
+     *)
+       echo " - Opcao desconhecida."
+     ;;
    esac
- }
+ done
+ 
+ echo ""
+ echo ""
+ echo ""
 
- fn_dracut_module()
- {
-  echo -e " - Alterando dracut hostonly : \c"
-  dracut -N -f > /dev/null 2>&1
-  fn_rc
-  echo -e " - Inserindo linha em \"/etc/dracut.conf\" : \c"
-  sleep 1
-  echo "hostonly=\"no\"" >> /etc/dracut.conf 2>/dev/null
-  fn_rc
- }
-
- # Execucao
- fn_grub_version
- fn_grub_config
- fn_dracut_module
+ df -hPT 
 
  echo ""
- echo "Fim!"
  echo ""
+ echo " Fim! "
