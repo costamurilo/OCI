b3fa038b54590528636258a9cb8bd5f31a9dd3db
costamurilo <42807980+costamurilo@users.noreply.github.com>
2024-11-14 15:54:45 -0300

    Update CriaLVM_StorageGateway.sh

--- CriaLVM_StorageGateway.sh
+++ CriaLVM_StorageGateway.sh
@@ -3,7 +3,7 @@
  # Criado por : Murilo Costa
  # Email : murilo.c.costa@oracle.com
  # Data : 17/02/2023 
- # Script usado para criar estrutura de filesystems para instalação do OCI Storage Gateway
+
 
  # Pre requisitos
  # Criar vg com minimo de 700GB
@@ -11,10 +11,10 @@
  # Executar estes comandos antes de executar o script
  # mkdir -p /tmp/sg
  # cd /tmp/sg
- # echo "/ocisg          ocisg    1 " >> /tmp/sg/lista.lst
+ # echo "/ocisg          ocisg    10 " >> /tmp/sg/lista.lst
  # echo "/ocisg/cache    cache    500" >> /tmp/sg/lista.lst
- # echo "/ocisg/metadata metadata 80" >> /tmp/sg/lista.lst
- # echo "/ocisg/log      log      20 " >> /tmp/sg/lista.lst
+ # echo "/ocisg/metadata metadata 100" >> /tmp/sg/lista.lst
+ # echo "/ocisg/log      log      30 " >> /tmp/sg/lista.lst
 
  # Instalacao do Storage gateway
  # Filesystem da instacao : /ocisg
@@ -27,38 +27,131 @@
 
  HOMEDIR="/tmp/sg"
    LISTA="${HOMEDIR}/lista.lst"
-    NAME="$(cat ${LISTA}  | awk '{ print $2 }')"
-     FSS="$(cat lista.lst | awk '{ print $1 }')"
+    NAME="ocisg cache metadata log"
+     FSS="/ocisg /ocisg/cache /ocisg/metadata /ocisg/log"
 
+ ######################################################################################################################
  clear
 
+ # Funcoes
+ fn_rc(){
+ 	     if [ ${?} -eq 0 ]
+ 	     	 then
+ 	     	 	 echo "OK"
+ 	     	 else
+ 	     	 	 echo "NOK"
+ 	     fi
+ }
+
  val_vg(){
+ 	   echo " Validando VG \"${VGNAME}\" "
+ 	   sleep 1
 
  	   VALIDA="$(vgs | grep -w ${VGNAME} | wc -l)"
  	   if [ ${VALIDA} -eq 0 ]
  	     then
  	     	 echo ""
  	     	 echo " Vg \"${VGNAME}\" inexistente."
+ 	     	 sleep 1
  	     	 echo ""
  	     	 echo -e " Verifique o nome do VG e digite novamente.\c"
+ 	     	 sleep 1
+ 	     	 echo " Fim! "
  	     	 exit 100
  	     else
- 	     	 echo " VG \"${VGNAME}\" validado!"
- 	     	 echo ""
+ 	     	 VGSIZE="$(vgs | grep -w ${VGNAME} | awk '{ print $6 }' | cut -f 2 -d \< | cut -f 1 -d .)"
+ 	     	 if [ ${VGSIZE} -gt 700 ]
+ 	     	 	 then
+ 	     	 	 	 echo " Volume do VG \"${VGNAME}\" OK! [ ${VGSIZE} ]"
+ 	     	 	 	 sleep 1
+    	     	 echo " VG \"${VGNAME}\" validado!"
+    	     	 sleep 1
+ 	     	     echo ""
+ 	     	   else
+ 	     	   	 echo " Volume do VG \"${VGNAME}\" insuficiente : [ ${VGSIZE} ] "
+ 	     	   	 echo " Para continuar utilize um VG com pelo menos 700GB."
+ 	     	   	 echo " Fim."
+ 	     	   	 #exit 200
+ 	     	 fi
  	   fi
  }
 
+ val_dir(){
+         echo ""
+         echo " Checando se o dir [/tmp/sg ] existe : "
+         sleep 1
+         DIR="/tmp/sg"
+         if [  -e ${DIR} ]
+           then
+           	 echo " Diretorio [ /tmp/sg ] existente."
+           	 sleep 1
+           	 echo  -e " Limpando diretorio : \c"
+           	 unalias rm > /dev/null 2>&1
+           	 rm -f ${DIR}/* > /dev/null 2>&1
+           	 sleep 1
+           	 fn_rc
+           else
+           	 echo " Diretorio [ /tmp/sg ] nao existente."
+           	 echo  -e " Criando diretorio : \c"
+           	 mkdir -p /tmp/sg > /dev/null 2>&1
+           	 fn_rc
+         fi
+
+ }
+
+ ######################################################################################################################
  # Execucao do script
+ 
+ # Valida o diretorio /tmp/sg
+ val_dir
+
  echo ""
  vgs
  echo ""
 
  echo -e " Digite o nome do VG onde a estrutura do Storage Gateway deve ser criado : \c"
  read VGNAME
+ echo ""
+ 
+ # Valida o tamanho do VG
+ val_vg
+
+ # Coleta o tamanho desejado de cada filesystem
+
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg\" [ 10 ] : \c"
+ read OCISG_SIZE
+ if [ -z ${OCISG_SIZE} ]
+ 	 then
+ 	 	 OCISG_SIZE="10"
+ fi
+
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg/cache\" [ 500 ] : \c"
+ read OCISG_CACHE_SIZE
+ if [ -z ${OCISG_CACHE_SIZE} ]
+ 	 then
+ 	 	 OCISG_CACHE_SIZE="500"
+ fi
+
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg/metadata\" [ 100 ] : \c"
+ read OCISG_METADATA_SIZE
+ if [ -z ${OCISG_METADATA_SIZE} ]
+ 	 then
+ 	 	 OCISG_METADATA_SIZE="100"
+ fi
+
+ echo -e " Digite o tamanho desejado para o Filesystem \"/ocisg/log\" [ 30 ] : \c"
+ read OCISG_LOG_SIZE
+ if [ -z ${OCISG_LOG_SIZE} ]
+ 	 then
+ 	 	 OCISG_LOG_SIZE="30"
+ fi
 
  echo ""
- val_vg
- echo ""
+ # --------------------------------------------------------------------------------------------------------------------
+
+
+
+
 
  # Criacao dos LVs
  echo " - Criando estrutura de Filesystem do Storage Gateway"
@@ -70,29 +163,33 @@
    	 ocisg)
        echo " - Criando LV lv_sg_${LV} "
        sleep 1
-       lvcreate -y -L10G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       lvcreate -y -L${OCISG_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
        mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
+       
      ;;
 
      cache)
        echo  " - Criando LV lv_sg_${LV} "
        sleep 1
-       lvcreate -y -L500G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       lvcreate -y -L${OCISG_CACHE_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
        mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
+       
      ;;
 
        metadata)
        echo  " - Criando LV lv_sg_${LV}  "
        sleep 1
-       lvcreate -y -L100G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       lvcreate -y -L${OCISG_METADATA_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
        mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
+       
      ;;
 
        log)
        echo  " - Criando LV lv_sg_${LV} "
        sleep 1
-       lvcreate -y -L30G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
+       lvcreate -y -L${OCISG_LOG_SIZE}G -n lv_sg_${LV} ${VGNAME} > /dev/null 2>&1
        mkfs.xfs /dev/mapper/${VGNAME}-lv_sg_${LV} -f > /dev/null 2>&1
+       
      ;;
 
      *)
@@ -150,6 +247,7 @@
        echo  " - Compondo /etc/fstab  "
        $(echo "/dev/mapper/${VGNAME}-lv_sg_metadata    ${FS}    xfs    defaults 0  0" >> /etc/fstab)
        echo ""
+       
      ;;
 
        /ocisg/log)
@@ -164,6 +262,7 @@
        echo  " - Compondo /etc/fstab  "
        $(echo "/dev/mapper/${VGNAME}-lv_sg_log    ${FS}    xfs    defaults 0  0" >> /etc/fstab)
        echo ""
+       
      ;;
 
      *)
